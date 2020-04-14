ebcf046d1ba5a90959f705dd416fa21e5b61e4a6
peipengjie <1412892850@qq.com>
2020-04-14 11:50:51 +0800

    bug

--- app-mvc/src/main/java/com/datarecord/webapp/fillinatask/controller/SimpleExcelWrite.java
+++ app-mvc/src/main/java/com/datarecord/webapp/fillinatask/controller/SimpleExcelWrite.java
@@ -36,52 +36,51 @@ public class SimpleExcelWrite {
     private ExcelP excelP;
 
     //生成excel
-    @RequestMapping("/generateexcel")
+    @RequestMapping("/generateExcel")
     @ResponseBody
     @CrossOrigin(allowCredentials = "true")
-    public String generateexcel(
-            @RequestBody Map<String,Object> request
+    public String generateExcel(
+            @RequestBody Map<String, Object> request
     ) {
         try {
-        String  jobids = String.valueOf(request.get("jobid").toString());
-        Integer jobid = Integer.valueOf(jobids).intValue();
-        String  reportids =  request.get("reportid").toString();
-        Integer reportid = Integer.valueOf(reportids).intValue();
+            String jobids = String.valueOf(request.get("jobid").toString());
+            Integer jobid = Integer.valueOf(jobids).intValue();
+            String reportids = request.get("reportid").toString();
+            Integer reportid = Integer.valueOf(reportids).intValue();
 
-        List<Object> unitList =  new ArrayList<Object>();
-        unitList = (List<Object>) request.get("unitList");
-
-
-       // List<Jobstatus> unitList = (List<Jobstatus>) request.get("unitList");
+            List<Object>  unitList = (List<Object>) request.get("unitList");
 
 
-        //第一步创建workbook
-        HSSFWorkbook wb = new HSSFWorkbook();
-       String jobname =  fillinataskService.selectrcdjobconfig(jobid);           //查询出此任务名称   excel名称
-           //第二步创建sheet
-        for (int aa=0 ; aa<unitList.size();aa++){
-            LinkedTreeMap<String,Object> DS = (LinkedTreeMap<String, Object>) unitList.get(aa);
-         //   String UNI = unitList.
-           String job_unit_name = fillinataskService.selectrcdjobunitconfig(DS.get("unitId").toString());//查出任务组名称 sheet页名称
-            HSSFSheet sheet  =  wb.createSheet(job_unit_name);     //创建sheet页
-            //第三步创建行row:添加表头0行
-            HSSFRow row = sheet.createRow(0);
-            HSSFCellStyle style = wb.createCellStyle();
-            List fldids = new ArrayList();       //需要字段id
-            List<String> lsls = (List<String>) DS.get("fldList");
-            for (Object fldlie : lsls){
-                fldids.add(fldlie);
+            // List<Jobstatus> unitList = (List<Jobstatus>) request.get("unitList");
+
+
+            //第一步创建workbook
+            HSSFWorkbook wb = new HSSFWorkbook();
+            String jobname = fillinataskService.selectrcdjobconfig(jobid);           //查询出此任务名称   excel名称
+            //第二步创建sheet
+            for (int aa = 0; aa < unitList.size(); aa++) {
+                LinkedTreeMap<String, Object> DS = (LinkedTreeMap<String, Object>) unitList.get(aa);
+                //   String UNI = unitList.
+                String job_unit_name = fillinataskService.selectrcdjobunitconfig(DS.get("unitId").toString());//查出任务组名称 sheet页名称
+                HSSFSheet sheet = wb.createSheet(job_unit_name);     //创建sheet页
+                //第三步创建行row:添加表头0行
+                HSSFRow row = sheet.createRow(0);
+                HSSFCellStyle style = wb.createCellStyle();
+                List fldids = new ArrayList();       //需要字段id
+                List<String> lsls = (List<String>) DS.get("fldList");
+                for (Object fldlie : lsls) {
+                    fldids.add(fldlie);
+                }
+                List<Lieming> LS = fillinataskService.selectrcdreportdatajob(jobid, reportid, DS.get("unitId").toString(), fldids);
+                HSSFCell cell = null;
+                for (int j = 0; j < LS.size(); j++) {
+                    cell = row.createCell(j);
+                    cell.setCellValue(LS.get(j).getFld_name());      //第四步创建单元格
+                    cell.setCellStyle(style);
+                    row = sheet.createRow(j + 1);  //创建行
+                    row.createCell(j).setCellValue(LS.get(j).getRecord_data());     //创建单元格并且添加数据 // 第五步插入数据
+                }
             }
-            List<Lieming>  LS = fillinataskService.selectrcdreportdatajob(jobid,reportid,DS.get("unitId").toString(),fldids);
-            HSSFCell cell =null;
-              for (int j = 0 ;j<LS.size();j++){
-                  cell = row.createCell(j);
-                  cell.setCellValue(LS.get(j).getFld_name());      //第四步创建单元格
-                  cell.setCellStyle(style);
-                  row = sheet.createRow(j + 1);  //创建行
-                  row.createCell(j).setCellValue(LS.get(j).getRecord_data());     //创建单元格并且添加数据 // 第五步插入数据
-              }
-        }
             //利用反射加载properties文件
         /*   String url = ExcelP.class.getResource("/excelpath.properties").toURI().getPath();
             InputStream inStream;
@@ -91,14 +90,14 @@ public class SimpleExcelWrite {
             url = prop.getProperty("url");*/
             //第六步将生成excel文件保存到指定路径下
 
-            FileOutputStream fout = new FileOutputStream(""+excelP.getPath()+""+jobname+".xls");
+            FileOutputStream fout = new FileOutputStream("" + excelP.getPath() + "" + jobname + ".xls");
             wb.write(fout);
             fout.close();
         } catch (Exception e) {
             e.printStackTrace();
-            return  JsonSupport.makeJsonResultStr(JsonResult.RESULT.FAILD, "生成excel文件失败", null, "error");
+            return JsonSupport.makeJsonResultStr(JsonResult.RESULT.FAILD, "生成excel文件失败", null, "error");
         }
-        return  JsonSupport.makeJsonResultStr(JsonResult.RESULT.SUCCESS, "生成excel文件成功", null, "success");
+        return JsonSupport.makeJsonResultStr(JsonResult.RESULT.SUCCESS, "生成excel文件成功", null, "success");
     }
 
 }
